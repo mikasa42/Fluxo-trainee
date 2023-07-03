64b465f03b8c77df37a19431bc5bcfed187eeffa
mikasa42 <mikasa201642@gmail.com>
2023-07-03 00:36:20 -0300

    Dificuldades com a biblioteca prompt-sync

--- capacitacao_1/Sistema.js
+++ capacitacao_1/Sistema.js
@@ -3,8 +3,16 @@ import { Consulta } from "./Consulta.js";
 import { Animal } from "./Animal.js";
 import { Funcionario } from "./Funcionario.js";
 
-
+"use strict";
 
 let n1 = new Cliente(1,'Mikaela','July','Sim');
+let n2 = new Funcionario(1,'Mirella',1234);
 
 console.log(`Id: ${n1.id} - Nome: ${n1.nome} - Pet: ${n1.pet} - Fidelidade: ${n1.fidelidade}`);
+console.log(n2.senhaF);
+
+const prompt = require("prompt-sync")({sigint:true});
+//let x;
+const x = parseInt(prompt('Digite um valor qualquer:'));
+//x = entrada("Digite um valor qualquer:");
+console.log(`x = ${x}`);
