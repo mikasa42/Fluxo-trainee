2394bdee62578bce9f1db4519b5dfcf7340c8afd
mikasa42 <mikasa201642@gmail.com>
2023-07-03 01:10:23 -0300

    Construindo Login

--- capacitacao_1/Sistema.js
+++ capacitacao_1/Sistema.js
@@ -1,7 +1,56 @@
-import { Cliente } from "./Cliente.js";
-import { Consulta } from "./Consulta.js";
-import { Animal } from "./Animal.js";
-import { Funcionario } from "./Funcionario.js";
+class Cliente{
+    constructor(idCliente,nomeCliente,petCliente,fidelidadeCliente){
+
+        this.id=idCliente;
+        this.nome=nomeCliente;
+        this.pet=petCliente;
+        this.fidelidade=fidelidadeCliente;
+    }
+
+}
+class Consulta{
+    constructor(idCliente,nomeCliente,nomePet,nomeFuncionario,statusConsulta,dataConsulta){
+
+        this.id=idCliente;
+        this.nome=nomeCliente;
+        this.nomepet=nomePet;
+        this.nomefuncionario=nomeFuncionario;
+        this.statusconsulta=statusConsulta;
+        this.dataconsulta=dataConsulta;
+    }
+}
+class Animal{
+    constructor(idPet,nomePet,consultasPet){
+
+        this.id=idPet;
+        this.nome=nomePet;
+        this.consulta=consultasPet;
+    }
+}
+class Funcionario{
+    #senha;
+    constructor(idFuncionario,nomeFuncionario,senhaFuncionario){
+
+        this.id=idFuncionario;
+        this.nome=nomeFuncionario;
+        this.#senha=senhaFuncionario;
+    }
+    //encapsulamento
+    get senhaF(){
+        return this.#senha;
+    }
+    set senhaF(senhaS){
+
+        this.#senha = senhaS;
+    }
+    login(nomeF,senhaFun){
+        if(nomeF == this.nome && senhaFun == this.#senha)
+            return 1;
+        else
+            return 0;
+    }
+
+}
 
 "use strict";
 
@@ -12,7 +61,28 @@ console.log(`Id: ${n1.id} - Nome: ${n1.nome} - Pet: ${n1.pet} - Fidelidade: ${n1
 console.log(n2.senhaF);
 
 const prompt = require("prompt-sync")({sigint:true});
-//let x;
-const x = parseInt(prompt('Digite um valor qualquer:'));
-//x = entrada("Digite um valor qualquer:");
+let x = parseInt(prompt('Digite um valor qualquer:'));
 console.log(`x = ${x}`);
+let i = 0;
+while(i!=2){
+    console.log("\n\n")
+    console.log('******************************\n');
+    console.log('****** 0 - Login       ********\n');
+    console.log('****** 1 - Cadastro    ********\n');
+    console.log('****** 2 - Sair         *******\n');
+    i = parseInt(prompt('Digite a sua opcao:'));
+    if(i==0){
+        let nomeF = parseInt(prompt('Digite o seu nome:'));
+        let senhaFun = parseInt(prompt('Digite a sua senha:'));
+        let verificacao = n2.login(nomeF,senhaFun);
+        if(verificacao == 1){
+            console.log("Bem-vindo");
+        }
+        else{
+            console.log(verificacao);
+        }
+
+    }
+
+
+}
