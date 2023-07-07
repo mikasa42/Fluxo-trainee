0df3d3a5a1a00c71cba599feae425df40cfe7a1a
mikasa42 <mikasa201642@gmail.com>
2023-07-07 00:02:47 -0300

    Entrega finalizada

--- Capacitacao_2/js/script.js
+++ Capacitacao_2/js/script.js
@@ -5,5 +5,5 @@ function carregar(){
     let email = input1.value;
     let senha = input2.value;
     
-    alert("Voce confirma esses dados ? "+ " Email: "+ email + "Senha: "+ senha);
+    alert("Voce confirma esses dados ? \n"+ "Email: "+ email + "\n" + "Senha: "+ senha);
 }
